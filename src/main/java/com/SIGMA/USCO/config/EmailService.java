5ac8dd345d44eee4ef26b89491b3f9f5b54f667c
NicolasPerezU <u20221204357@usco.edu.co>
2025-12-08 10:08:36 -0500

    login

---
+++ src/main/java/com/SIGMA/USCO/config/EmailService.java
@@ -0,0 +1,22 @@
+package com.SIGMA.USCO.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String message) {
+
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(message);
+        mailSender.send(email);
+    }
+}
